2ec8bd98f896ba7ddc423fa801c7fece70765341
Louis Andrada <luis.procopioandrada@georgebrown.ca>
2026-02-26 02:21:13 -0500

    more updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./layouts/**/*.{html,js,ts}",
     "./content/**/*.{html,md}",
@@ -8,12 +9,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
-module.exports = {
-  darkMode: 'class',
-  content: [...],
-  theme: { extend: {} },
   plugins: [],
 }
