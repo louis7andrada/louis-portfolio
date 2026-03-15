52e1202652c3f5b0eac5ff8e9f4148eb181534e3
Louis Andrada <luis.procopioandrada@georgebrown.ca>
2026-03-15 17:30:21 -0400

    website updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,19 @@ module.exports = {
     "./layouts/**/*.html",
     "./content/**/*.md",
     "./assets/**/*.{css,js,ts}",
-
     "./themes/portfolio-theme/layouts/**/*.html",
     "./themes/portfolio-theme/assets/**/*.{css,js,ts}",
     "./themes/portfolio-theme/content/**/*.md"
+  ],
+  safelist: [
+    'dark',
+    'burger', 'burger.open', 
+    'filter-dropdown', 'filter-dropdown.open',
+    'featured-fade', 'featured-fade.hidden',
+    'single-image-box', 'single-image', 'single-details',
+    'prevBtnDesktop', 'nextBtnDesktop',
+    'mobileMenu', 'mobileMenu.open',
+    'moonIcon', 'sunIcon',
   ],
   theme: {
     extend: {},
